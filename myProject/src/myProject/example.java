ed6264d0b54be30f1f0458691efbe40aa58b863b
Ilias13 <hlias_mast@hotmail.com>
2016-09-15 00:32:13 -0400

    Changed example program

--- myProject/src/myProject/example.java
+++ myProject/src/myProject/example.java
@@ -4,6 +4,9 @@ public class example
 {
  public static void main(String [] args)
  {
-	 System.out.println("Hello world");
+	 for(int i=0; i<4; i++)
+	 {	 
+	  System.out.println("Hello world");
+	 }
  }
 }
